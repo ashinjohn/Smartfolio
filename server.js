498ac7861943efe88ebad7398bad2b1a9c5d3616
Ashin Basil John <ashinbasiljohn@gmail.com>
2017-02-16 11:02:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+<link href="/ui/style.css" rel="stylesheet" />
 
 var app = express();
 app.use(morgan('combined'));
