bec4d3d8162e871426d58a9676af10972e02a396
Ashin Basil John <ashinbasiljohn@gmail.com>
2017-02-19 12:58:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,11 @@ app.get('/:articleName',function(req,res){
   res.send(createtemplate(articles[articleName]));
 });
 
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -66,7 +71,10 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});    
+});
+
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
